bc73673ee2a71ee6638dfe5faeaab6b1bca40775
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2025-02-27 23:35:46 -0300

    .

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,21 @@
 'use client'
-
 import React from 'react'
 import Input from '../Input/Input'
+import Button from '../Button/Button';
 
 export default function Login() {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
+  async function logar() {
+    
+  }
+
   return (
     <article>
       <Input label='Username' id='username' type='text' setValue={setUsername} value={username}/>
       <Input label='Password' id='password' type='text' setValue={setPassword} value={password}/>
+      <Button onClick={logar}>LogaRr</Button>
     </article>
   )
 }
