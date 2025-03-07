296269e8a67a15805a9038d2014dcaafad79e8df
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2025-03-06 23:27:39 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <h2>Exercício 3</h2>
       <Link href={'/login'}>Login</Link>
       <h2>Exercício 4</h2>
-      <Link href={'/teste'}>Teste</Link>
+      <Link href={'/teste'}>TesteAAA</Link>
     </main>
   );
 }
